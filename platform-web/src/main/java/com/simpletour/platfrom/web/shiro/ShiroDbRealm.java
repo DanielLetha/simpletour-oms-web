d8bd1871210b1675e259f4c4898b00bf8db19925
Hawk <595541377@qq.com>
2016-04-20 18:25:57 +0800

    合并代码

--- platform-web/src/main/java/com/simpletour/platfrom/web/shiro/ShiroDbRealm.java
+++ platform-web/src/main/java/com/simpletour/platfrom/web/shiro/ShiroDbRealm.java
@@ -1,13 +1,17 @@
 package com.simpletour.platfrom.web.shiro;
 
-import com.simpletour.domain.company.Administrator;
-import com.simpletour.service.company.IAdministratorService;
+
+import com.simpletour.domain.company.Employee;
+import com.simpletour.service.company.IEmployeeService;
+import com.simpletour.service.company.IRoleService;
 import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationException;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.crypto.hash.Md5Hash;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import java.io.Serializable;
@@ -16,47 +20,39 @@ import java.util.Objects;
 import java.util.Optional;
 
 /**
- * Created by zt on 2015/11/20.
+ * Created by mario on 2016/4/12.
  */
+@Component
 public class ShiroDbRealm extends AuthorizingRealm {
 
     @Resource
-    private IAdministratorService administratorService;
+    private IEmployeeService employeeService;
 
-    /**
-     * 验证登录
-     * @param authcToken
-     * @return
-     * @throws AuthenticationException
-     */
+    @Resource
+    private IRoleService roleService;
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        ShiroDbRealm.ShiroUser shiroUser = (ShiroDbRealm.ShiroUser) principals.getPrimaryPrincipal();
+        Optional<Employee> user = employeeService.queryEmployeeByJobNo(Integer.parseInt(shiroUser.getJobNO()));
+        if (!user.isPresent()) throw new AuthorizationException();
+        return new SimpleAuthorizationInfo();
+    }
+
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authcToken) throws AuthenticationException {
         UsernamePasswordToken token = (UsernamePasswordToken) authcToken;
-        Optional<Administrator> adminOpt = administratorService.findAdminByJobNo(token.getUsername());
-        if (!adminOpt.isPresent()) {
-            throw new AuthenticationException("账户不存在");
+        Optional<Employee> employeeOptional = employeeService.queryEmployeeByJobNo(Integer.valueOf(token.getUsername()));
+        if (employeeOptional.isPresent() && !employeeOptional.get().getDel()) {
+            token.setPassword(new Md5Hash(token.getPassword(), employeeOptional.get().getSalt()).toBase64().toCharArray());
+            if (!Arrays.equals(token.getPassword(), employeeOptional.get().getPasswd().toCharArray())) {
+                throw new AuthenticationException("账户和密码错误");
+            }
+            return new SimpleAuthenticationInfo(new ShiroUser(employeeOptional.get().getId()
+                    , String.valueOf(employeeOptional.get().getJobNo()), employeeOptional.get().getName())
+                    , employeeOptional.get().getPasswd(), getName());
         }
-        if (!Administrator.Status.inservice.equals(adminOpt.get().getStatus())) {
-            throw new AuthenticationException("当前管理员已经离职");
-        }
-        char[] newPassword = new Md5Hash(token.getPassword(), adminOpt.get().getSalt()).toBase64().toCharArray();
-        if (!Arrays.equals(newPassword, adminOpt.get().getPassword().toCharArray())) {
-            throw new AuthenticationException("登录失败，请检查用户名与密码");
-        }
-        token.setPassword(newPassword);
-        Administrator admin = adminOpt.get();
-        return new SimpleAuthenticationInfo(new ShiroUser(admin.getId(), admin.getJobNo(), admin.getName())
-                , admin.getPassword(), admin.getName());
-    }
-
-    /**
-     * 权限初始化，因为当前系统不存在权限，所以是全权限的
-     * @param principals
-     * @return
-     */
-    @Override
-    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
-        return new SimpleAuthorizationInfo();
+        throw new AuthenticationException("登录失败");
     }
 
     /**
