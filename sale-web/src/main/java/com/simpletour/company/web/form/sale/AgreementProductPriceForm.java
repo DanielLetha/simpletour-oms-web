dbe718ef816e3f803043529479f860953cba8810
liangfei <liangfei@simpletour.com>
2016-04-22 10:33:20 +0800

    增加销售端的controller

---
+++ sale-web/src/main/java/com/simpletour/company/web/form/sale/AgreementProductPriceForm.java
@@ -0,0 +1,16 @@
+package com.simpletour.company.web.form.sale;
+
+import com.simpletour.company.web.form.support.BaseForm;
+
+/**
+ * @Brief :  ${用途}
+ * @Author: liangfei/liangfei@simpletour.com
+ * @Date :  2016/4/22 10:26
+ * @Since ： ${VERSION}
+ * @Remark: ${Remark}
+ */
+public class AgreementProductPriceForm extends BaseForm {
+
+
+
+}
