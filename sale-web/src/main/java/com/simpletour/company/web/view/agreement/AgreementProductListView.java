563ca8330b24ff7d7dea775c284a1196f9070511
WangLin <wl@simpletour.com>
2016-04-23 14:15:15 +0800

    去掉注释

--- sale-web/src/main/java/com/simpletour/company/web/view/agreement/AgreementProductListView.java
+++ sale-web/src/main/java/com/simpletour/company/web/view/agreement/AgreementProductListView.java
@@ -1,23 +1,22 @@
-//package com.simpletour.company.web.view.agreement;
-//
-//import com.simpletour.commons.data.domain.DomainPage;
-//import com.simpletour.company.web.query.agreement.AgreementProductQuery;
-//import com.simpletour.company.web.view.BaseListView;
-//import com.simpletour.domain.company.Company;
-//import com.simpletour.domain.sale.AgreementProduct;
-//
-///**
-// * Author:  wangLin
-// * Mail  :  wl@simpletour.com
-// * Date  :  2016/4/22.
-// * Remark:
-// */
-//public class AgreementProductListView extends BaseListView<AgreementProductElementView>{
-//
-//    public AgreementProductListView(DomainPage<AgreementProduct> domainPage, AgreementProductQuery query){
-//        super(domainPage,query);
-//        if (domainPage.getDomains() != null && !domainPage.getDomains().isEmpty()){
-//            domainPage.getDomains().stream().forEach(agreementProduct -> subViews.add(new AgreementProductElementView(agreementProduct)));
-//        }
-//    }
-//}
+package com.simpletour.company.web.view.agreement;
+
+import com.simpletour.commons.data.domain.DomainPage;
+import com.simpletour.company.web.query.agreement.AgreementProductQuery;
+import com.simpletour.company.web.view.BaseListView;
+import com.simpletour.domain.sale.AgreementProduct;
+
+/**
+ * Author:  wangLin
+ * Mail  :  wl@simpletour.com
+ * Date  :  2016/4/22.
+ * Remark:
+ */
+public class AgreementProductListView extends BaseListView<AgreementProductElementView>{
+
+    public AgreementProductListView(DomainPage<AgreementProduct> domainPage, AgreementProductQuery query){
+        super(domainPage,query);
+        if (domainPage.getDomains() != null && !domainPage.getDomains().isEmpty()){
+            domainPage.getDomains().stream().forEach(agreementProduct -> subViews.add(new AgreementProductElementView(agreementProduct)));
+        }
+    }
+}
